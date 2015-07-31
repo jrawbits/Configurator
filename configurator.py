afe359ac5970f209e94b000461e8b91f1a7c5dff
Jeremy Raw <jeremy@jeremyraw.com>
2015-07-31 15:08:43 -0400

    Fixed formatting for 'info' block in configuration

--- configurator.py
+++ configurator.py
@@ -15,43 +15,44 @@ tool_config = {
     "info" : {
         "name" : "Tool Configurator",
         "text" : """
-The "Configurator" is the place to begin if you're trying to figure
+<p>The "Configurator" is the place to begin if you're trying to figure
 out how to build an interface description for a Non-Motorized Toolkit
 (NMTK) tool(what we call a "tool configuration" or just a "tool
-config").
+config").</p>
 
-This "do-nothing" tool demonstrates the essential properties of an
+<p>This "do-nothing" tool demonstrates the essential properties of an
 NMTK tool configuration file.  While it does nothing useful (though
 some might find it amusing), it does exercise all the Tool API
 elements, including status updates and multiple result files.  Behind
 the scenes, it also gives you a skeleton for a tool and some helper
-functions that will make tool development easier.
+functions that will make tool development easier.</p>
 
-Speaking of easy, building an NMTK tool is just easy.  We knocked out
+<p>Speaking of easy, building an NMTK tool is just easy.  We knocked out
 this Configurator tool in just a couple of days.  Writing an NMTK tool
 is way easier than writing a spreadsheet (nobody should ever use
-spreadsheets again).
+spreadsheets again).</p>
 
-The hardest part of building a tool is constructing the configuration
+<p>The hardest part of building a tool is constructing the configuration
 file so the user can present you with the data you need.  But even a
 configuration file is not especially hard, and you can use the
-Configurator to learn all you need to know about it.
+Configurator to learn all you need to know about it.</p>
 
-Configurations have five main parts, which broadly track with what you
-would expect to find in a reproducible research repository (q.v.):
+<p>Configurations have five main parts, which broadly track with what you
+would expect to find in a reproducible research repository (q.v.).</p>
 
-The <strong>"info"</strong> section contains an overview of the
+<ol>
+<li>The <strong>"info"</strong> section contains an overview of the
 tool: its name, a block of HTML marked-up text that describes it --
-that's what you're reading now -- and version information.
+that's what you're reading now -- and version information.</li>
 
-The <strong>"sample"</strong> section describes sample data and a sample
+<li>The <strong>"sample"</strong> section describes sample data and a sample
 job configuration.  You can use that for testing your tool while you're
 developing it so you don't have to repeatedly build job setups by hand,
 and it will allow your users to experience your tool to decide if it might
 work for them, without having to go through an extensive data preparation
-step of their own.  That section includes 
+step of their own.</li>
 
-The <strong>"documentation"</strong> section provides links or files
+<li>The <strong>"documentation"</strong> section provides links or files
 that the user can open to learn more about the tool.  This tool has a
 link to its "home base" in Github, so you can download any of the tool
 code to look at up close.  You can also retrieve the configuration
@@ -60,11 +61,13 @@ clicking the "Configuration" link.  Finally, you can also download a
 couple of PDF files:  the latest version of the Tool Configuration
 specification, and the introductory document on how to write a tool.
 In short, the Configurator is your gateway to doing cool stuff with
-the Non-Motorized Toolkit!
+the Non-Motorized Toolkit!</li>
+</ol>
 
 The real action happens in the remaining two sections:
 
-The <strong>"input"</strong> section describes what the user has to
+<ol start="4">
+<li>The <strong>"input"</strong> section describes what the user has to
 provide to the tool in order to make it work: input files, fields
 within those files, parameters and coefficients, or even literal data
 to analyze (put a number into the Configurator's "Nth root" field and
@@ -72,14 +75,15 @@ see what comes back!). It says what kinds of files it expects and knows
 how to work with.  It ways what is required and what is optional.  The
 help text that goes with each of the input elements tells the user
 concisely what they need to put into that element; here in the Configurator,
-the text focuses on explaining the user interface properties.
+the text focuses on explaining the user interface properties.</li>
 
-The <strong>"output"</strong> section describes what the tool is going
+<li>The <strong>"output"</strong> section describes what the tool is going
 to return.  This section is not exactly optional because it gives
 useful information to the NMTK explaining what it is going to return.
 And it lets you give the user some ability to shape how the results
 are presented, for example by specifying alternate names for the
-result fields returned. 
+result fields returned.</li>
+</ol>
 """,
         "version" : "1.0"
     },
